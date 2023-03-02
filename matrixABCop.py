be15a197df7709a32ec818cf4f998592c29e6e73
bruski99 <114541522+bruski99@users.noreply.github.com>
2023-03-02 10:57:20 -0300

    Update matrixABCop.py

--- matrixABCop.py
+++ matrixABCop.py
@@ -1,30 +1,39 @@
-import numpy as np
-
-# prompt user to enter values for matrices A, B, and C
+# prompt user to enter values for matrix A
 print("Enter values for 2x2 matrix A:")
-a11 = float(input("a11: "))
-a12 = float(input("a12: "))
-a21 = float(input("a21: "))
-a22 = float(input("a22: "))
-A = np.array([[a11, a12], [a21, a22]])
+A = []
+for i in range(2):
+    row = []
+    for j in range(2):
+        row.append(float(input(f"a{i+1}{j+1}: ")))
+    A.append(row)
+print("Matrix A:\n", A)
 
+# prompt user to enter values for matrix B
 print("Enter values for 2x2 matrix B:")
-b11 = float(input("b11: "))
-b12 = float(input("b12: "))
-b21 = float(input("b21: "))
-b22 = float(input("b22: "))
-B = np.array([[b11, b12], [b21, b22]])
+B = []
+for i in range(2):
+    row = []
+    for j in range(2):
+        row.append(float(input(f"b{i+1}{j+1}: ")))
+    B.append(row)
+print("Matrix B:\n", B)
 
+# prompt user to enter values for matrix C
 print("Enter values for 2x2 matrix C:")
-c11 = float(input("c11: "))
-c12 = float(input("c12: "))
-c21 = float(input("c21: "))
-c22 = float(input("c22: "))
-C = np.array([[c11, c12], [c21, c22]])
+C = []
+for i in range(2):
+    row = []
+    for j in range(2):
+        row.append(float(input(f"c{i+1}{j+1}: ")))
+    C.append(row)
+print("Matrix C:\n", C)
 
-# compute the result of A*B*C
-result = np.dot(np.dot(A, B), C)
-
-# print the result
-print("Result of A*B*C:")
-print(result)
+# calculate the result of the three matrices
+result = []
+for i in range(2):
+    row = []
+    for j in range(2):
+        value = A[i][j] * B[i][j] - C[i][j]
+        row.append(value)
+    result.append(row)
+print("Result:\n", result)
